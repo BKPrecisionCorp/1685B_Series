3880ae05d21a189ccf7e80307e84b8fcca6eaac8
Ryan Roth <rroth@bkprecision.com>
2020-01-16 11:47:05 -0800

    Added port selection functionality to GUI program.

--- Python 3/gui/1685bSeries.py
+++ Python 3/gui/1685bSeries.py
@@ -1,13 +1,29 @@
 from tkinter import *
 import serial
+import serial.tools.list_ports
 import lib1685b
-
-ser = serial.Serial("/dev/ttyUSB0")
-ser.timeout = 0.1
 
 win = Tk()
 
+ports = serial.tools.list_ports.comports()
 
+port = StringVar()
+lb = Listbox(win, listvariable=port)
+i = 0
+for port, desc, hwid in sorted(ports):
+    i = i+1
+    lb.insert(i, port)
+lb.pack()
+
+ser = ""
+
+def setPort():
+    print(sorted(ports)[lb.curselection()[0]].device)
+    global ser
+    ser = serial.Serial(sorted(ports)[lb.curselection()[0]].device)
+    ser.timeout = 0.1
+
+Button(win, text="Set Port", command=setPort).pack()
 
 def setVoltage():
     val = voltscale.get()
@@ -20,14 +36,13 @@ def setCurrent():
     lib1685b.setCurrent(ser, val)
 
 
-    
-currscale = Scale(win, from_=0, to=10, resolution=0.1, orient=HORIZONTAL, length=300)
-currscale.pack()
-Button(win, text="Set Current", command=setCurrent).pack()
-
 voltscale = Scale(win, from_=0, to=36, resolution=0.1, orient=HORIZONTAL, length=300)
 voltscale.pack()
 Button(win, text="Set Voltage", command=setVoltage).pack()
+
+currscale = Scale(win, from_=0, to=10, resolution=0.1, orient=HORIZONTAL, length=300)
+currscale.pack()
+Button(win, text="Set Current", command=setCurrent).pack()
 
 vlab = StringVar()
 vlab.set("voltage")
